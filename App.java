db141ac15e7cc5f9696e85394cb8fa9379d8818e
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-18 13:25:20 +0200

    Add private methods addTaskToList(), markTaskToList(), removeTaskToList(), update main() method in App module

--- App.java
+++ App.java
@@ -8,10 +8,10 @@ public class App{
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);;
     String command;
-    String taskToDo;
+
     String endPorgram;
     TodoList taskList = new TodoList();
-    Integer taskToChange;
+    Integer taskToChange = null;
 
     do {
       System.out.println("Please specify a command [list, add, mark, remove, archive]: ");
@@ -22,25 +22,15 @@ public class App{
 
       } else if (command.toLowerCase().equals("add")) {
   
-        System.out.println("Add an item: ");
-        taskToDo = in.nextLine();
-  
-        TodoItem newTask = new TodoItem(taskToDo);
-        taskList.addTaskToList(newTask);
-        System.out.println("Item added.");
+        taskList = addTaskToList(taskList, in);
 
       }  else if (command.toLowerCase().equals("mark")) {
   
-        System.out.println("Which one task you want to mark as completed: ");
-        taskToChange = in.nextInt();
-  
-        taskList.markTask(taskToChange);
+        taskList = markTaskToList(taskList, in, taskToChange);
   
       } else if (command.toLowerCase().equals("remove")) {
   
-        System.out.println("Which one task you want to remove: ");
-        taskToChange = in.nextInt();
-        taskList.removeTask(taskToChange);
+        taskList = removeTaskToList(taskList, in, taskToChange);
 
       } else if (command.toLowerCase().equals("archive")) {
         taskList.archiveTasks();
@@ -56,6 +46,34 @@ public class App{
     } while (!endPorgram.toLowerCase().equals("yes"));
 
     in.close();
+  }
+
+  private static TodoList addTaskToList(TodoList taskList, Scanner in) {
+    String taskToDo;
+    System.out.println("Add an item: ");
+    taskToDo = in.nextLine();
+
+    TodoItem newTask = new TodoItem(taskToDo);
+    taskList.addTaskToList(newTask);
+    System.out.println("Item added.");
+    return taskList;
+  }
+
+  private static TodoList markTaskToList(TodoList taskList, Scanner in, Integer taskToChange) {
+    System.out.println("Which one task you want to mark as completed: ");
+    taskToChange = in.nextInt();
+
+    taskList.markTask(taskToChange);
+    return taskList;
+
+  }
+
+  private static TodoList removeTaskToList(TodoList taskList, Scanner in, Integer taskToChange) {
+    System.out.println("Which one task you want to remove: ");
+    taskToChange = in.nextInt();
+    taskList.removeTask(taskToChange);
+    return taskList;
+
   }
 
 }
