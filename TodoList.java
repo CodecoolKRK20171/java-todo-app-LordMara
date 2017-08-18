855c4ac0dfe03503db13c7c4e42512d46930fa0e
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-18 13:03:20 +0200

    Implement constructor and methods getListToDo(), setListToDo(), addTaskToList(), displayTasksList(), markTask(), removeTask(), archiveTasks() in TodoList module

--- TodoList.java
+++ TodoList.java
@@ -1,7 +1,82 @@
+import java.util.ArrayList;
+
 /**
  * Class representing a todo list.
  * TodoList object should aggregate TodoItem objects.
  */
 public class TodoList{
+
+    private ArrayList<TodoItem> listToDo;
+
+    public TodoList() {
+        this.listToDo = new ArrayList<TodoItem>();
+    }
+
+    public ArrayList<TodoItem> getListToDo() {
+        return this.listToDo;
+    }
+
+    public void setListToDo(ArrayList<TodoItem> listToDo) {
+        this.listToDo = listToDo;
+    }
+
+    public void addTaskToList(TodoItem task) {
+        listToDo.add(task);
+    }
+
+    public void displayTasksList() {
+        Integer count = 1;
+
+        for (TodoItem task : listToDo) {
+
+            System.out.println(String.format("%d. %s", count, task.createPrintableTaskToDo()));
+            count += 1;
+        }
+    }
+
+    public void markTask(int taskToMarkNumber) {
+        taskToMarkNumber -= 1;
+        for (int i = 0; i < listToDo.size(); i++) {
+            if (taskToMarkNumber == (i)) {
+
+                if (!listToDo.get(i).getTaskDoneStatus()) {
+                    listToDo.get(i).setTaskDoneStatus(true);
+
+                } else {
+
+                    listToDo.get(i).setTaskDoneStatus(false);
+                }
+            }
+        }
+    }
+
+    public void removeTask(int taskToRemoveNumber) {
+        ArrayList<TodoItem> temp = new ArrayList<TodoItem>();
+        taskToRemoveNumber -= 1;
+        for (int i = 0; i < listToDo.size(); i++) {
+
+            if (taskToRemoveNumber == (i)) {
+
+                continue;
+
+                } else {
+
+                    temp.add(listToDo.get(i));
+                }
+        }
+
+        this.listToDo = temp;
+    }
+
+    public void archiveTasks() {
+
+        for (int i = 0; i < listToDo.size(); i++) {
+
+            if (listToDo.get(i).getTaskDoneStatus() == true) {
+
+                listToDo.remove(i);
+            }
+        }
+    }
 
 }
