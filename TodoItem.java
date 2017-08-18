86c44b394e030fbab3d4cf22f14ed146bbb4ac5e
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-18 13:01:36 +0200

    Implement constructor and methods getTaskToDo(), setTaskToDo(), getTaskDoneStatus(), setTaskDoneStatus() and createPrintableTaskToDo() in TodoItem module

--- TodoItem.java
+++ TodoItem.java
@@ -4,4 +4,40 @@
  */
 public class TodoItem{
 
+    private String taskToDo;
+    private boolean taskDoneStatus;
+
+    public TodoItem(String taskToDo) {
+        this.taskToDo = taskToDo;
+        this.taskDoneStatus = false;
+    }
+
+    public String getTaskToDo() {
+        return this.taskToDo;
+    }
+
+    public void setTaskToDo(String taskToDo) {
+        this.taskToDo = taskToDo;
+    }
+
+    public boolean getTaskDoneStatus() {
+        return this.taskDoneStatus;
+    }
+
+    public void setTaskDoneStatus(boolean taskDoneStatus) {
+        this.taskDoneStatus = taskDoneStatus;
+    }
+
+    public String createPrintableTaskToDo() {
+        String taskWithStatus = new String();
+
+        if (taskDoneStatus == false) {
+            taskWithStatus = String.format("[ ].%s", taskToDo);
+
+        } else if (taskDoneStatus == true) {
+            taskWithStatus = String.format("[X].%s", taskToDo);
+        }
+
+        return taskWithStatus;
+    }
 }
